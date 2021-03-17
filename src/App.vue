e949114f2022731a749410a3c8c81f9ed3534340
Tsubasa Kondo <tsubasa.k111@gmail.com>
2021-03-17 11:58:56 +0900

    feat: display message from server

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div>{{message}}</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -14,10 +15,13 @@ export default {
   components: {
     HelloWorld
   },
+  data: ()=> ({
+    message: "あーあー聞こえますか"
+  }),
   mounted(){
     axios.get("/api").then((res) => {
+      this.message = res.data;
       console.log(res.data);
-      
     })
   }
 
